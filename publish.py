0ac04f383050bed16902c4378cacfa916578de33
omegakid1902 <anhdungbk09@gmail.com>
2021-09-11 21:04:07 +0700

    Update publish.py

--- publish.py
+++ publish.py
@@ -1,18 +1,26 @@
 import os
 import frontmatter
 from shutil import copy
+import os
 
 if not os.path.exists('./docs'):
     os.mkdir('./docs')
 
+def files_publish(file):
+    if file.endswith(".md"):
+    with open(os.path.join(root, file), encoding="utf8") as f:
+            content = f.read()
+            metadata, content = frontmatter.parse(content)
+            if 'publish' in metadata.keys():
+                print("Copy publish files from zettelkasten to docs/")
+                print(os.path.join(root, file))
+                copy(os.path.join(root, file), './docs/')
+            else:
+                pass
+
+for file in os.listdir("./zettelkasten/"):
+    files_publish(file)
+
 for root, dirs, files in os.walk("./zettelkasten/Zet/"):
-    for file in files:
-        if file.endswith(".md"):
-            with open(os.path.join(root, file), encoding="utf8") as f:
-                content = f.read()
-                metadata, content = frontmatter.parse(content)
-                if 'publish' in metadata.keys():
-                    print(os.path.join(root, file))
-                    copy(os.path.join(root, file), './docs/')
-                else:
-                    pass
+    files_publish(file)
+
