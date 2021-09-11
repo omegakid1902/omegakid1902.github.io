bcd091c777d50e45289fad891b916ba39fe2a917
omegakid1902 <anhdungbk09@gmail.com>
2021-09-11 21:05:38 +0700

    Update publish.py

--- publish.py
+++ publish.py
@@ -9,14 +9,14 @@ if not os.path.exists('./docs'):
 def files_publish(file):
     if file.endswith(".md"):
     with open(os.path.join(root, file), encoding="utf8") as f:
-            content = f.read()
-            metadata, content = frontmatter.parse(content)
-            if 'publish' in metadata.keys():
-                print("Copy publish files from zettelkasten to docs/")
-                print(os.path.join(root, file))
-                copy(os.path.join(root, file), './docs/')
-            else:
-                pass
+        content = f.read()
+        metadata, content = frontmatter.parse(content)
+        if 'publish' in metadata.keys():
+            print("Copy publish files from zettelkasten to docs/")
+            print(os.path.join(root, file))
+            copy(os.path.join(root, file), './docs/')
+        else:
+            pass
 
 for file in os.listdir("./zettelkasten/"):
     files_publish(file)
